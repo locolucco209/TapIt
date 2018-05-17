0fc5920c33c1ce368d24cf8b9cf1c82d3d4f431b
mohammadse2 <mpowerelectronic@gmail.com>
2018-05-16 19:01:44 -0700

    add social login page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { HashRouter as Router, Route, Link} from 'react-router-dom';
 import HashRouter from 'react-router-dom'
-import {Landing_page, Add_user, Claim_brewery} from './components/index.js'
+import {Landing_page, Add_user, Claim_brewery,social_login} from './components/index.js'
 
 import './App.css';
 
@@ -13,6 +13,7 @@ class App extends Component {
           <Route exact path="/" component={Landing_page}/>
           <Route exact path="/add_user" component={Add_user}/>
           <Route exact path="/claim_brewery" component={Claim_brewery}/>
+          <Route exact path="/social_login" component={social_login}/>
         </div>
       </Router>
     );
